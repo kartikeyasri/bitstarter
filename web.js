626bb0df395295aceee396d3e2403c9e5f0a25ae
kartikeyasri <kartikeyarcc@gmail.com>
2013-07-06 07:46:05 +0000

    web.js changed

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   
-  var welcome = fs.readFileSync('index.html');
+  var buffer = new Buffer(fs.readFileSync('index.html'));
+  var welcome = buffer.toString('utf-8');
   response.send(welcome);
 });
 
